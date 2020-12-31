d9a8b340947e65ec9e446a8ed09f240b24de8fe9
JhonnyParraB <41595878+JhonnyParraB@users.noreply.github.com>
2020-12-31 18:29:52 -0500

    router commented

---
+++ Server/router.go
@@ -0,0 +1,14 @@
+package main
+
+/*
+func getRouter() *chi.Mux {
+	router := chi.NewRouter()
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.Logger)
+
+	var dayDataLoaderHandler handler.LoadDayDataHandler
+	router.Post("/load-day-data?date={date}", dayDataLoaderHandler.LoadDayData)
+
+	return router
+}
+*/
