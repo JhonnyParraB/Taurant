971a1fe06d3f9da99b51f9ff1f1be4c1b761d782
JhonnyParraB <41595878+JhonnyParraB@users.noreply.github.com>
2020-12-31 18:29:38 -0500

    main with router inside

--- Server/main.go
+++ Server/main.go
@@ -1,37 +1,34 @@
 package main
 
 import (
-	"io/ioutil"
+	"fmt"
 	"log"
 	"net/http"
 
-	jsoniter "github.com/json-iterator/go"
-
-	"./model"
-	"./repository"
+	"./handler"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 func main() {
-	var endpointCaseJSON = jsoniter.Config{TagKey: "endpoint"}.Froze()
-	var buyer_repository repository.BuyerRepositoryDGraph
-
-	response, err := http.Get("https://kqxty15mpg.execute-api.us-east-1.amazonaws.com/buyers")
-	handleError(err)
-	data, _ := ioutil.ReadAll(response.Body)
-	var buyers []model.Buyer
-	err = endpointCaseJSON.Unmarshal(data, &buyers)
-	for _, buyer := range buyers {
-		buyer_found := buyer_repository.FindById(buyer.ID)
-		if buyer_found == nil {
-			buyer_repository.Create(&buyer)
-		} else {
-			buyer_repository.Update(buyer_found.UID, &buyer)
-		}
-	}
+	router := getRouter()
+	fmt.Println("Server listen at : 4200")
+	http.ListenAndServe(":4200", router)
 }
 
 func handleError(err interface{}) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func getRouter() *chi.Mux {
+	router := chi.NewRouter()
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.Logger)
+
+	var dayDataLoaderHandler handler.LoadDayDataHandler
+	router.Post("/load-day-data", dayDataLoaderHandler.LoadDayData)
+
+	return router
 }
