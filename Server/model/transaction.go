bcc04c07875cdcdd89c49d5ba85490399f1c1584
JhonnyParraB <41595878+JhonnyParraB@users.noreply.github.com>
2020-12-31 18:29:23 -0500

    transaction_id tag corrected

--- Server/model/transaction.go
+++ Server/model/transaction.go
@@ -3,7 +3,7 @@ package model
 //Transaction type details
 type Transaction struct {
 	UID      string    `predicate:"uid,omitempty"`
-	ID       string    `predicate:"id,omitempty"`
+	ID       string    `predicate:"transaction_id,omitempty"`
 	Buyer    Buyer     `predicate:"is_made_by,omitempty"`
 	IP       string    `predicate:"ip,omitempty"`
 	Device   string    `predicate:"device,omitempty"`
