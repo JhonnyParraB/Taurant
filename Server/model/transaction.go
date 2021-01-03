f9dcde5d64b822dada83e856c65227a9637b2c5d
JhonnyParraB <41595878+JhonnyParraB@users.noreply.github.com>
2021-01-03 05:52:38 -0500

    Location changed in transaction, now is a struct

--- Server/model/transaction.go
+++ Server/model/transaction.go
@@ -5,7 +5,7 @@ type Transaction struct {
 	UID      string    `predicate:"uid,omitempty"`
 	ID       string    `predicate:"transaction_id,omitempty"`
 	Buyer    Buyer     `predicate:"is_made_by,omitempty"`
-	IP       string    `predicate:"ip,omitempty"`
+	Location Location  `predicate:"location,omitempty"`
 	Device   string    `predicate:"device,omitempty"`
 	Products []Product `predicate:"trade,omitempty"`
 }
