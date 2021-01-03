c86e8d1923df7983b562d98143c83c12542c6d73
JhonnyParraB <41595878+JhonnyParraB@users.noreply.github.com>
2021-01-03 05:50:54 -0500

    Buyer with shared ip

--- Server/model/buyer.go
+++ Server/model/buyer.go
@@ -6,5 +6,6 @@ type Buyer struct {
 	ID           string        `predicate:"buyer_id,omitempty"    endpoint:"id,omitempty"`
 	Name         string        `predicate:"buyer_name,omitempty"  endpoint:"name,omitempty"`
 	Age          int           `predicate:"age,omitempty"         endpoint:"age,omitempty"`
-	Transactions []Transaction `predicate:"perform,omitempty"`
+	Transactions []Transaction `predicate:"perform,omitempty"     endpoint:"transactions,omitempty"`
+	SharedIP     string        `predicate:"shared_ip,omitempty"   endpoint:"sharedIP,omitempty"`
 }
