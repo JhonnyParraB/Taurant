07afee9f050ef8af3e1f7b3b8809d944a6d16667
JhonnyParraB <41595878+JhonnyParraB@users.noreply.github.com>
2021-01-03 05:53:03 -0500

    Repository for location

---
+++ Server/repository/location_repository.go
@@ -0,0 +1,53 @@
+package repository
+
+import (
+	"encoding/json"
+
+	"../driver"
+	"../model"
+	jsoniter "github.com/json-iterator/go"
+)
+
+type LocationRepository interface {
+	Create(location *model.Location) string
+	FindByIP(ip string) *model.Location
+	Update(uid string, location *model.Location) string
+}
+
+type LocationRepositoryDGraph struct {
+}
+
+func (b LocationRepositoryDGraph) Create(location *model.Location) {
+	location.UID = "_:" + location.IP
+	driver.RunMutation(location)
+	*location = *(b.FindByIP(location.IP))
+}
+
+func (b LocationRepositoryDGraph) Update(uid string, location *model.Location) {
+	location.UID = uid
+	driver.RunMutation(location)
+}
+
+func (b LocationRepositoryDGraph) FindByIP(ip string) *model.Location {
+	query :=
+		`
+		{
+			findLocationByIP(func: eq(ip, "` + ip + `"), first: 1) {
+				uid
+				ip
+			}
+		}	
+	`
+	res := driver.RunQuery(query)
+	var locationsFound []model.Location
+	var objmap map[string]json.RawMessage
+	err := json.Unmarshal(res, &objmap)
+	handleError(err)
+	var predicateCaseJSON = jsoniter.Config{TagKey: "predicate"}.Froze()
+	err = predicateCaseJSON.Unmarshal(objmap["findLocationByIP"], &locationsFound)
+	handleError(err)
+	if len(locationsFound) > 0 {
+		return &locationsFound[0]
+	}
+	return nil
+}
