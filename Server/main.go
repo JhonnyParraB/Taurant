8b62f30ff904cfbdc548edd34c0ee0403e8bce7c
JhonnyParraB <41595878+JhonnyParraB@users.noreply.github.com>
2021-01-13 04:49:49 -0500

    Router moved

--- Server/main.go
+++ Server/main.go
@@ -3,10 +3,6 @@ package main
 import (
 	"log"
 	"net/http"
-
-	"./handler"
-	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
 )
 
 func main() {
@@ -16,20 +12,4 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-func getRouter() *chi.Mux {
-	router := chi.NewRouter()
-	router.Use(middleware.Recoverer)
-	router.Use(middleware.Logger)
-
-	var dayDataLoaderHandler handler.LoadDayDataHandler
-	var buyersHandler handler.BuyersHandler
-	dayDataLoaderHandler.Init()
-	router.Post("/load-day-data/{date}", dayDataLoaderHandler.LoadDayData)
-
-	//buyers
-	router.Get("/buyers", buyersHandler.GetBuyersBasicInformation)
-	router.Get("/buyers/{id}", buyersHandler.GetBuyerDetailedInformation)
-	return router
 }
--- Server/router.go
+++ Server/router.go
@@ -1,14 +1,24 @@
 package main
 
-/*
+import (
+	"./handler"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
+
 func getRouter() *chi.Mux {
 	router := chi.NewRouter()
 	router.Use(middleware.Recoverer)
 	router.Use(middleware.Logger)
 
 	var dayDataLoaderHandler handler.LoadDayDataHandler
-	router.Post("/load-day-data?date={date}", dayDataLoaderHandler.LoadDayData)
+	var buyersHandler handler.BuyersHandler
+	dayDataLoaderHandler.Init()
+
+	router.Post("/load-day-data/{date}", dayDataLoaderHandler.LoadDayData)
+
+	router.Get("/buyers", buyersHandler.GetBuyersBasicInformation)
+	router.Get("/buyers/{id}", buyersHandler.GetBuyerDetailedInformation)
 
 	return router
 }
-*/
